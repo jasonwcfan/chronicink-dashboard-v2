163538d68744c5341ecac14d6ec5c9f8d1e50da2
jasonwcfan <jason.wc.fan@gmail.com>
2016-10-27 20:35:56 -0400

    globalized body styling in main.css

--- client/components/intake/intake_form.js
+++ client/components/intake/intake_form.js
@@ -9,4 +9,12 @@ class IntakeForm extends Component {
     }
 }
 
+const styles = {
+    base: {
+        font: '300 14px/1.4 \'Helvetica Neue\', Helvetica, Arial, sans-serif',
+        background: '#eee',
+        margin: '0',
+        padding: '0'
+    }
+};
 export default IntakeForm;
