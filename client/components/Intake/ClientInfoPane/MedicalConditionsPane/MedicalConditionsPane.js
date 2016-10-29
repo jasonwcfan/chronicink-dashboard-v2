94ad4597d0c12e2edd21226834f3c31321514678
jasonwcfan <jason.wc.fan@gmail.com>
2016-10-29 19:55:18 -0400

    added conditions and styling

--- client/components/Intake/ClientInfoPane/MedicalConditionsPane/MedicalConditionsPane.js
+++ client/components/Intake/ClientInfoPane/MedicalConditionsPane/MedicalConditionsPane.js
@@ -4,25 +4,28 @@ import conditions from './conditions';
 
 const style = {
     container: {
-        display: 'flex'
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        width: '100%'
+
     },
     item: {
-        width: 200
+        width: 200,
+        padding: 10
     }
 };
 
 const conditionsList = [];
 for (let i = 0; i < conditions.length; i++) {
-    conditionsList.push(<Checkbox label={conditions[i]} key={i} />);
+    conditionsList.push(<Checkbox style={style.item} key={i} label={conditions[i]} />);
 }
 
 class MedicalConditionsPane extends Component {
     render() {
         return(
             <div style={style.container}>
-                <div style={style.item}>
-                    {conditionsList}
-                </div>
+                {conditionsList}
             </div>
         )
     }
