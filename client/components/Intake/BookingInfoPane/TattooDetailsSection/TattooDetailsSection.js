eaadd4e6aae3e5dc6dc2932871d9715ae8331dfc
jasonwcfan <jason.wc.fan@gmail.com>
2016-10-30 16:49:07 -0400

    added tattoo detail text fields

--- client/components/Intake/BookingInfoPane/TattooDetailsSection/TattooDetailsSection.js
+++ client/components/Intake/BookingInfoPane/TattooDetailsSection/TattooDetailsSection.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import TextField from 'material-ui/TextField';
 import StyleSelector from './StyleSelector';
 import PlacementSelector from './PlacementSelector';
 
@@ -11,6 +12,15 @@ const style = {
     datePicker: {
         marginLeft: 5,
         marginRight: 5
+    },
+    radioGroup: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        maxWidth: 650
+    },
+    radioButton: {
+        display: 'inline-block',
+        padding: 10,
     }
 };
 
@@ -20,7 +30,30 @@ class TattooDetailsSection extends Component {
             <div>
                 <h2>Tattoo Details</h2>
                 <StyleSelector /><br />
-                <PlacementSelector />
+                <PlacementSelector /><br />
+                <TextField style={style.textField} floatingLabelText="Size" />
+                <h3>Colour</h3>
+                <RadioButtonGroup style={style.radioGroup} name='colour' defaultSelected='blackAndGrey'>
+                    <RadioButton
+                        value='blackAndGrey'
+                        label='Black & grey'
+                        style={style.radioButton}
+                    />
+                    <RadioButton
+                        value='colour'
+                        label='Colour'
+                        style={style.radioButton}
+                    />
+                    <RadioButton
+                        value='blackAndGreyWithTouchesOfColour'
+                        label='Black & grey with touches of colour'
+                        style={style.radioButton}
+                    />
+                </RadioButtonGroup>
+                <TextField style={style.textField} floatingLabelText="Skin Tone" fullWidth={true} multiLine={true} /><br />
+                <TextField style={style.textField} floatingLabelText="Subject" fullWidth={true} multiLine={true} /><br />
+                <TextField style={style.textField} floatingLabelText="Feel" fullWidth={true} multiLine={true} /><br />
+                <TextField style={style.textField} floatingLabelText="Background" fullWidth={true} multiLine={true} />
             </div>
         )
     }
