44b812d29512fefca80e0b3c6e8354c85d4f0593
jasonwcfan <jason.wc.fan@gmail.com>
2016-11-17 10:13:30 -0500

    fetch only pending consult forms

--- client/main.js
+++ client/main.js
@@ -25,7 +25,7 @@ Tracker.autorun(() => {
     store.dispatch({
         type: 'UPDATE_INTAKE_LIST',
         id: 'intakeList',
-        data: IntakeForm.find().fetch()
+        data: IntakeForm.find({consultPending: true}).fetch()
     })
 });
 
