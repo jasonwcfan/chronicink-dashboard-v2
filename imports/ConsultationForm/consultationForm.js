440eadf3b15800c3185c53313be1e9b60536056f
jasonwcfan <jason.wc.fan@gmail.com>
2016-11-27 20:06:39 -0500

    only save pass id, label and value for consultationForms

--- imports/ConsultationForm/consultationForm.js
+++ imports/ConsultationForm/consultationForm.js
@@ -5,7 +5,7 @@ Meteor.methods({
     'consultationForm.insert': function(form) {
         console.log(form);
         return ConsultationForm.insert({
-            fields: form.fields,
+            fields: form.fields.map((field) => ({id: field.id, label: field.label, value: field.value})),
             sessions: form.sessions
         });
     },
