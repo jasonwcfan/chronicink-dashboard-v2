688cd700684c88019de9207579bd702ab79147fd
jasonwcfan <jason.wc.fan@gmail.com>
2017-03-02 12:48:03 -0500

    added phone number text

--- client/components/IntakeForm/CallUsStep/CallUsStep.js
+++ client/components/IntakeForm/CallUsStep/CallUsStep.js
@@ -20,8 +20,8 @@ class CallUsStep extends Component {
     render() {
         return (
             <div style={{textAlign: 'center'}}>
-                <h2>You\'re not finished yet!</h2>
-                <p>If you are in the shop, please let a staff member know that you\'ve completed the form. Otherwise, please give us a call when you are ready to leave your deposit.</p>
+                <h2>You're not finished yet!</h2>
+                <p>If you are in the shop, please let a staff member know that you've completed the form. Otherwise, please give us a call when you are ready to leave your deposit.</p>
 
                 <RaisedButton
                     href="tel:416-544-0311"
@@ -31,6 +31,7 @@ class CallUsStep extends Component {
                     style={styles.button}
                     icon={<CommunicationCall />}
                 />
+                <p style={{marginTop: 10}}>416-544-0311</p>
 
                 <p style={{textAlign: 'center'}}>NOTE: The submission of this form does not imply that an appointment or booking has been made. Please contact us if you would like to continue with your consultation.
                     This form will be deleted in 24 hours if no official booking and deposit has been completed.
