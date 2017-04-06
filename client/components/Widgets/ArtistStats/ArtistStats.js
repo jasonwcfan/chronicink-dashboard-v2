ab04f81ebc87b7d877ea78f5ba7d800256edc6e0
Ron <ron.suprun@gmail.com>
2017-04-06 16:35:12 -0400

    Walkthrough fix of loading issue in Artist Stats widget
    Artist hours now saved to database

--- client/components/Widgets/ArtistStats/ArtistStats.js
+++ client/components/Widgets/ArtistStats/ArtistStats.js
@@ -56,7 +56,6 @@ class ArtistStats extends Component {
         super(props);
 
         this.state = {
-            artistStats: [],
             timeFrame: 30
         };
 
@@ -68,41 +67,13 @@ class ArtistStats extends Component {
         this._handleRefreshArtistStats(props.artists, 30);
     }
 
-    _handleReceiveArtistStats(artist, artistIndex, err, hoursBooked) {
-        const nextArtistStats = this.state.artistStats.slice();
-        nextArtistStats[artistIndex] = {
-            calendarID: artist.calendarID,
-            name: artist.name,
-            status: err,
-            hoursBooked: hoursBooked,
-            loading: false
-        };
-
-        this.setState({
-            artistStats: nextArtistStats
-        })
-    }
 
 
     _handleRefreshArtistStats(artists, timeFrame) {
-        const nextArtistStats = this.state.artistStats.slice();
 
+        // Calls back-end method to store artist hours in database
         artists.forEach((artist, idx) => {
-            nextArtistStats[idx] = {
-                calendarID: artist.calendarID,
-                name: artist.name,
-                status: null,
-                hoursBooked: null,
-                loading: true
-            };
-
-            Meteor.call('artist.getHoursBooked', artist.calendarID, timeFrame, (err, res) => {
-                this._handleReceiveArtistStats(artist, idx, err, res);
-            });
-        });
-
-        this.setState({
-            artistStats: nextArtistStats
+            Meteor.call('artist.getHoursBooked', artist.calendarID, timeFrame);
         });
     }
 
@@ -116,12 +87,12 @@ class ArtistStats extends Component {
 
     _renderArtistStats() {
         if (this.props.subReady) {
-            return this.state.artistStats.map((artistStat) => {
-                const message = artistStat.status ? 'Error' : artistStat.hoursBooked + ' hours booked';
+            return this.props.artists.map((artist) => {
+                const message = artist.hours ?  artist.hours + ' hours booked': 'Error';
                 return (
-                    <ListItem key={artistStat.calendarID} primaryText={artistStat.name}>
+                    <ListItem key={artist.calendarID} primaryText={artist.name}>
                         <div style={style.listItemContainer} >
-                            <div style={style.listItemRightLabel}>{artistStat.loading ?
+                            <div style={style.listItemRightLabel}>{artist.loading ?
                                 <CircularProgress
                                     size={16}
                                 />
--- imports/Artist/artist.js
+++ imports/Artist/artist.js
@@ -4,11 +4,21 @@ import { Mongo } from 'meteor/mongo';
 Meteor.methods({
     'artist.getHoursBooked': function(calendarID, timeFrame) {
         // Timeframe given in days (e.g. 60 = 2 months)
+
         if (Meteor.isServer) {
-            const getHours = Meteor.wrapAsync(GCalendar.getBookedHours);
-            return getHours(calendarID, timeFrame);
+            GCalendar.getBookedHours(calendarID, timeFrame, Meteor.bindEnvironment((err, hours) => {
+
+                // Update the hours of the artist with matching calendarID in the Artist collection
+                Artist.update({calendarID}, {$set:{hours}},(err,response) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
+            }));
+
+
         }
-    }
+    },
 });
 
 export default Artist = new Mongo.Collection('artist');
