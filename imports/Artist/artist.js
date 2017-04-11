599407838de600bb50611a6019cad5d2a1159b7a
jasonwcfan <jason.wc.fan@gmail.com>
2017-04-10 21:15:24 -0400

    add ability to refresh artist stats manually

--- imports/Artist/artist.js
+++ imports/Artist/artist.js
@@ -1,29 +1,40 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
+function getBookedHours(artist, timeFrame) {
+    GCalendar.getBookedHours(artist.calendarID, timeFrame, Meteor.bindEnvironment((err, hours) => {
+
+        // Create key value pair for # of hours booked within specified timeframe
+        let obj = {};
+        obj['hoursIn' + String(timeFrame) + 'Days'] = hours;
+        timeFrame = String(timeFrame);
+
+        // Update the hours of the artist with matching calendarID in the Artist collection
+        Artist.update({calendarID: artist.calendarID}, {$set:obj},(err,response) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    }));
+}
+
 Meteor.methods({
-    'artist.getHoursBooked': function(calendarID, timeFrame) {
+    'artist.getHoursBooked': function(timeFrame) {
         // Timeframe given in days (e.g. 60 = 2 months)
-
         if (Meteor.isServer) {
-            GCalendar.getBookedHours(calendarID, timeFrame, Meteor.bindEnvironment((err, hours) => {
-
-                // Create key value pair for # of hours booked within specified timeframe
-                let obj = {};
-                obj['hoursIn' + String(timeFrame) + 'Days'] = hours;
-                timeFrame = String(timeFrame);
-
-                // Update the hours of the artist with matching calendarID in the Artist collection
-                Artist.update({calendarID}, {$set:obj},(err,response) => {
-                    if (err) {
-                        console.log(err)
-                    }
-                })
-            }));
-
-
+            const timeFrames = [7, 14, 30, 60, 90];
+            const artists = Artist.find().fetch();
+            artists.forEach((artist) => {
+                if (timeFrame) {
+                    getBookedHours(artist, timeFrame);
+                } else {
+                    timeFrames.forEach((elem) => {
+                        getBookedHours(artist, elem);
+                    })
+                }
+            });
         }
-    },
+    }
 });
 
 export default Artist = new Mongo.Collection('artist');
