63d0dea9cc1fcc28d3e1f3979b68f9c6f1bcbb79
Yomi Osamiluyi <yomexzo@gmail.com>
2017-04-12 11:26:11 -0400

    Added extra fields to enter exact size dimensions for tattoos

--- client/components/BookingForm/TattooDetailsTab/TattooDetailsTab.js
+++ client/components/BookingForm/TattooDetailsTab/TattooDetailsTab.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import Toggle from 'material-ui/Toggle';
 import ValidatedTextField from '../../Inputs/ValidatedTextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
@@ -8,6 +9,17 @@ import { ValidatedAutoComplete } from '../../Inputs';
 import ArtistSelector from '../ArtistRecommendations';
 
 const style = {
+    flexedDivContainer: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'baseline'
+    },
+    switch: {
+        width: '250px',
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 15
+    },
     textField: {
         display: 'block'
     },
@@ -33,11 +45,89 @@ class TattooDetailsTab extends Component {
     constructor(props) {
         super(props);
         this._renderForm = this._renderForm.bind(this);
+        this.processSizeWidthAndHeight = this._processSizeWidthAndHeight.bind(this);
+        this.sizeWidthAndHeight = {
+            width: {
+                errorText: '',
+                touched: false,
+                value: '',
+                label: 'Width (Inches)',
+            },
+            height: {
+                errorText: '',
+                touched: false,
+                value: '',
+                label: 'Height (Inches)',
+            },
+        };
+        this.state = {
+            useActualSize: false
+        }
     }
     
     _renderForm(fields) {
         return fields.map((field) => {
             switch (field.inputType) {
+                case 'size':
+                    const textFieldStyle = Object.assign(style.textField, { width: '256px' });
+                    return (
+                        <div style={style.flexedDivContainer} key={'some-key'}>
+                            {this.state.useActualSize ?
+                                <div style={style.flexedDivContainer} key={'some-key-2'}>
+                                    <ValidatedTextField
+                                        style={Object.assign(textFieldStyle, { width: '123px' })}
+                                        defaultValue={this.sizeWidthAndHeight.width.value}
+                                        name={'size_width'}
+                                        key={'size_width'}
+                                        floatingLabelText={this.sizeWidthAndHeight.width.label}
+                                        onFieldChange={this.processSizeWidthAndHeight}
+                                        fullWidth={true}
+                                        required={field.required}
+                                        errorText={this.sizeWidthAndHeight.width.errorText}
+                                        touched={this.sizeWidthAndHeight.width.touched}
+                                    />
+                                    <ValidatedTextField
+                                        style={Object.assign(textFieldStyle, { width: '123px' })}
+                                        defaultValue={this.sizeWidthAndHeight.height.value}
+                                        name={'size_height'}
+                                        key={'size_height'}
+                                        floatingLabelText={this.sizeWidthAndHeight.height.label}
+                                        onFieldChange={this.processSizeWidthAndHeight}
+                                        fullWidth={true}
+                                        required={true}
+                                        errorText={this.sizeWidthAndHeight.height.errorText}
+                                        touched={this.sizeWidthAndHeight.height.touched}
+                                    />
+                                </div>
+                            :
+                                <ValidatedTextField
+                                    style={textFieldStyle}
+                                    defaultValue={this.props.formValues[field.id].value}
+                                    name={field.id}
+                                    key={field.id}
+                                    floatingLabelText={field.label}
+                                    onFieldChange={this.props.onFieldChange}
+                                    fullWidth={true}
+                                    required={field.required}
+                                    errorText={this.props.formValues[field.id].errorText}
+                                    touched={this.props.formValues[field.id].touched}
+                                />
+
+                            }
+                            <Toggle
+                                name={'use_actual_size'}
+                                key={'use_actual_size'}
+                                label="Use Exact Measurements"
+                                defaultToggled={this.state.useActualSize}
+                                style={style.switch}
+                                labelStyle={{width: 'auto'}}
+                                labelPosition={'right'}
+                                onToggle={(event, checked) => {
+                                    this.setState({'useActualSize': checked});
+                                }}
+                            />
+                        </div>
+                    );
                 case 'textField':
                     // console.log(this.props.formValues[field.id].value);
                     return (
@@ -147,6 +237,27 @@ class TattooDetailsTab extends Component {
         });
     }
     
+    _processSizeWidthAndHeight(id, value, errorText) {
+        if(id == 'size_width') {
+            this.sizeWidthAndHeight.width.value = value;
+            this.sizeWidthAndHeight.width.errorText = errorText;
+            this.sizeWidthAndHeight.width.touched = true;
+        } else if(id == 'size_height') {
+            this.sizeWidthAndHeight.height.value = value;
+            this.sizeWidthAndHeight.height.errorText = errorText;
+            this.sizeWidthAndHeight.height.touched = true;
+        }
+
+        const size = `${this.sizeWidthAndHeight.width.value} x ${this.sizeWidthAndHeight.height.value} (Inches)`;
+        let error = '';
+
+        if(this.sizeWidthAndHeight.width.errorText || this.sizeWidthAndHeight.height.errorText) {
+            error = 'Size is required';
+        }
+
+        this.props.onFieldChange('size', size, error);
+    }
+
     render() {
         return (
             <div style={this.props.style}>
