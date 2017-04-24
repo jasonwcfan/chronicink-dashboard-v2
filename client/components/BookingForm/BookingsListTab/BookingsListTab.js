df8ccb3c51beed482c95cb712f2bfad62278e175
Yomi Osamiluyi <yomexzo@gmail.com>
2017-04-24 19:04:36 -0400

    changed presentation required to yes/no instead of toggle

--- client/components/BookingForm/BookingsListTab/BookingsListTab.js
+++ client/components/BookingForm/BookingsListTab/BookingsListTab.js
@@ -4,6 +4,7 @@ import MenuItem from 'material-ui/MenuItem';
 import Toggle from 'material-ui/Toggle';
 import ValidatedTextField from '../../Inputs/ValidatedTextField';
 import BookingsList from './BookingsList';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 
 
 const style = {
@@ -12,6 +13,17 @@ const style = {
     },
     presentationToggle: {
         maxWidth: 360,
+    },
+    presentationLabel: {
+        display: 'inline-block',
+        marginRight: 105,
+        lineHeight: '29px',
+        verticalAlign: 'super'
+    },
+    radioItem: {
+        width: 'auto',
+        display: 'inline-block',
+        marginRight: 25
     }
 };
 
@@ -24,12 +36,30 @@ class BookingsListTab extends Component {
         return (
             <div style={this.props.style}>
                 <h2>Sessions</h2>
-                <Toggle
-                    style={style.presentationToggle}
-                    label='Presentation Required'
-                    toggled={this.props.presentationRequired}
-                    onToggle={this.props.togglePresentationRequired}
-                />
+                <div>
+                    <div style={style.presentationLabel}>Presentation Required</div>
+                    <RadioButtonGroup
+                        style={{display: 'inline-block'}}
+                        name={'presentationRequired'}
+                        valueSelected={this.props.presentationRequired}
+                        onChange={this.props.togglePresentationRequired}
+                    >
+                        <RadioButton
+                            style={style.radioItem}
+                            iconStyle={{marginRight: 10}}
+                            value={true}
+                            label={'Yes'}
+                            key={'yes'}
+                        />
+                        <RadioButton
+                            style={style.radioItem}
+                            iconStyle={{marginRight: 10}}
+                            value={false}
+                            label={'No'}
+                            key={'no'}
+                        />
+                    </RadioButtonGroup>
+                </div>
                 <BookingsList bookings={this.props.bookings} deleteBooking={this.props.deleteBooking} onSubmitBooking={this.props.onSubmitBooking} />
             </div>
         )
