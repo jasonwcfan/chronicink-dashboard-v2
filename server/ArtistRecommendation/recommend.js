817b7d40a64154e718e9963146581a131e81a8c4
Ron <ron.suprun@gmail.com>
2017-05-24 13:26:26 -0400

    removed comments

--- server/ArtistRecommendation/recommend.js
+++ server/ArtistRecommendation/recommend.js
@@ -4,14 +4,9 @@ recommendationEngine = {
 
         // normalize input variables between 0 and 1 so we can assign them weights
         let n_booking_volume = 1 - normalize(hoursIn60Days, max_booking_volume, min_booking_volume);
-        console.log('#### n_booking_volume: ', n_booking_volume);
-
-        // take 33 as the cut-off for penalizing lower preferences
         let n_style_preference = (tattooStylePreference - 33)/67;
-        console.log('#### n_style_preference: ', n_style_preference);
-
         let n_soonest_opening = normalize(daysUntilEarliestOpening, max_soonest_opening, min_soonest_opening);
-        console.log('#### n_soonest_opening: ', n_soonest_opening);
+
         // apply the weights here
         let score = (0.5 * n_booking_volume) + (1 * n_style_preference) + (2 * n_soonest_opening);
 
